96f10b5f3c826a222bf4a363967b178cbd4d117e
Nadav Hollander <nadavhollander@Nadavs-MacBook-Pro.local>
2017-06-11 02:06:47 -0700

    yet another dumb mistake

--- src/StateEngine.js
+++ src/StateEngine.js
@@ -149,7 +149,7 @@ class StateEngine {
             case 'raa-english':
               session.reply("Dharma Risk Assessment Ltd. will message you " +
                 "shortly with further instructions.  Tap 'Recent' to reach the recent messages screen -- they may have messaged you already!")
-              var url = RAA_ROOT + session.get("address") + "/" + session.get("paymentAddress");
+              var url = RAA_ROOT + "/" + session.get("address") + "/" + session.get("paymentAddress");
               console.log("Requesting from " + url);
               request(url, function(error, response, body) {
                 console.log(error);
